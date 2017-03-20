c477d0a2d796972d3d22508e4e213a1cb75440ea
Ammon Casey <ammon@caseyohana.com>
2017-03-20 09:11:24 -1000

    CompanyNameType

--- src/StructType/CompanyNameType.php
+++ src/StructType/CompanyNameType.php
@@ -11,6 +11,42 @@ use WsdlToPhp\PackageBase\AbstractStructBase;
 class CompanyNameType extends AbstractStructBase
 {
     /**
+     * Used to provide the company common name.
+     *
+     * @property string $CompanyShortName
+     */
+    protected $CompanyShortName;
+
+    /**
+     * The travel sector the company is associated with, such as air, car and hotel.
+     * Refer to OpenTravel Code List Travel Sector (TVS).
+     *
+     * @property string $TravelSector
+     */
+    protected $TravelSector;
+
+    /**
+     * Identifies a company by the company Code.
+     *
+     * @property string $Code
+     */
+    protected $Code;
+
+    /**
+     * Identifies the context of the identifying Code, such as DUNS, IATA or internal
+     * Code, etc.
+     *
+     * @property string $CodeContext
+     */
+    protected $CodeContext;
+
+    /**
+     * The country location of the company headquarters.Example: us
+     *
+     * @property string $CountryCode
+     */
+    protected $CountryCode;
+    /**
      * The Division
      * @var
      */
@@ -27,11 +63,150 @@ class CompanyNameType extends AbstractStructBase
      * @param  $division
      * @param  $department
      */
-    public function __construct( $division = null,  $department = null)
+    public function __construct($companyShortName = null, $travelSector = null, $code = null, $codeContext = null, $countryCode = null, $division = null,  $department = null)
     {
-        $this->setDivision($division)
+        $this->setCompanyShortName($companyShortName)
+             ->setTravelSector($travelSector)
+             ->setCode($code)
+             ->setCodeContext($codeContext)
+             ->setCountryCode($countryCode)
+             ->setDivision($division)
              ->setDepartment($department);
     }
+    /**
+     * Gets as CompanyShortName
+     *
+     * Used to provide the company common name.
+     *
+     * @return string
+     */
+    public function getCompanyShortName()
+    {
+        return $this->CompanyShortName;
+    }
+
+    /**
+     * Sets a new CompanyShortName
+     *
+     * Used to provide the company common name.
+     *
+     * @param string $CompanyShortName
+     * @return self
+     */
+    public function setCompanyShortName($CompanyShortName)
+    {
+        $this->CompanyShortName = $CompanyShortName;
+        return $this;
+    }
+
+    /**
+     * Gets as TravelSector
+     *
+     * The travel sector the company is associated with, such as air, car and hotel.
+     * Refer to OpenTravel Code List Travel Sector (TVS).
+     *
+     * @return string
+     */
+    public function getTravelSector()
+    {
+        return $this->TravelSector;
+    }
+
+    /**
+     * Sets a new TravelSector
+     *
+     * The travel sector the company is associated with, such as air, car and hotel.
+     * Refer to OpenTravel Code List Travel Sector (TVS).
+     *
+     * @param string $TravelSector
+     * @return self
+     */
+    public function setTravelSector($TravelSector)
+    {
+        $this->TravelSector = $TravelSector;
+        return $this;
+    }
+
+    /**
+     * Gets as Code
+     *
+     * Identifies a company by the company Code.
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->Code;
+    }
+
+    /**
+     * Sets a new Code
+     *
+     * Identifies a company by the company Code.
+     *
+     * @param string $Code
+     * @return self
+     */
+    public function setCode($Code)
+    {
+        $this->Code = $Code;
+        return $this;
+    }
+
+    /**
+     * Gets as CodeContext
+     *
+     * Identifies the context of the identifying Code, such as DUNS, IATA or internal
+     * Code, etc.
+     *
+     * @return string
+     */
+    public function getCodeContext()
+    {
+        return $this->CodeContext;
+    }
+
+    /**
+     * Sets a new CodeContext
+     *
+     * Identifies the context of the identifying Code, such as DUNS, IATA or internal
+     * Code, etc.
+     *
+     * @param string $CodeContext
+     * @return self
+     */
+    public function setCodeContext($CodeContext)
+    {
+        $this->CodeContext = $CodeContext;
+        return $this;
+    }
+
+    /**
+     * Gets as CountryCode
+     *
+     * The country location of the company headquarters.Example: us
+     *
+     * @return string
+     */
+    public function getCountryCode()
+    {
+        return $this->CountryCode;
+    }
+
+    /**
+     * Sets a new CountryCode
+     *
+     * The country location of the company headquarters.Example: us
+     *
+     * @param string $CountryCode
+     * @return self
+     */
+    public function setCountryCode($CountryCode)
+    {
+        $this->CountryCode = $CountryCode;
+        return $this;
+    }
+
     /**
      * Get Division value
      * @return |null
